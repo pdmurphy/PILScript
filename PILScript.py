55ae616ff3f7c0d0e0b46c83b24edf53a12a0dbc
Patrick Murphy <pdmurphy28@gmail.com>
2025-03-13 21:32:33 -0400

    Reads file of the filepath passed as argument and prints out. Also comments for future. testing append as well

--- PILScript.py
+++ PILScript.py
@@ -7,29 +7,48 @@ if len(sys.argv) > 1:
 else:
     anyArguments = False
 
-def setArgs(pil_csv_arg):
-    global pil_csv_filepath
-    pil_csv_filepath = pil_csv_arg
+global convertedFromCSVToOutput
+convertedFromCSVToOutput = []
+
+def setArgs(PILCSVArg):
+    global PILCSVFilepath
+    PILCSVFilepath = PILCSVArg
 
 if(anyArguments):
-    parser = argparse.ArgumentParser(description="Generates P&D image collage from a list of Ids.", add_help=False)
+    parser = argparse.ArgumentParser(description="Converts CSV to formatted text for PIL.", add_help=False)
     #spacer
-	inputGroup = parser.add_argument_group("Input")
+    inputGroup = parser.add_argument_group("Input")
     inputGroup.add_argument("--pil_csv", required=True, help="Path to PIL csv file")
     helpGroup = parser.add_argument_group("Help")
     helpGroup.add_argument("-h", "--help", action="help", help="Displays this help message and exits.")
     args = parser.parse_args()
     setArgs(args.pil_csv)
 
-def readIdFile(pilfilePath):
-    with open(pilfilePath) as csvfile:
+def readIdFile(PILFilePath):
+    with open(PILFilePath) as csvfile:
         csvReader = csv.reader(csvfile, delimiter=',')
         rowcount = 0
         for row in csvReader:
+            print(row)
+            convertedFromCSVToOutput.append("blah") #testing append
+            #convertedFromCSVToOutput += row
             #if(rowcount >= 1):
+            #use \n for newlines
                 #print("Your pil file is incorrect")
                 #exit()
                 # not robust and could be worked around but don't want to deal with it as I don't need it
             #else:
                 #separateIds(row)
                 #rowcount += 1
+
+def createOutputFile():
+    file = open("pil_script_output.txt", "w")
+    #yadda yadda 
+#notes for writing the file later. Also could just be a printout intitially as well.
+## Method 1
+#f = open("Path/To/Your/File.txt", "w")   # 'r' for reading and 'w' for writing
+#f.write("Hello World from " + f.name)    # Write inside file 
+#f.close()                                # Close file 
+
+readIdFile(PILCSVFilepath)
+print(''.join(convertedFromCSVToOutput))
